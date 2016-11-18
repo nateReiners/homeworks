0c5063b2407a0660189436002659d190751c1dae
Nate Reiners <nate.reiners@gmail.com>
2016-11-17 22:55:25 -0800

    Add map methods

---
+++ W1D4/exercises3_map.rb
@@ -0,0 +1,29 @@
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, val)
+    @map.each_with_index do |pair, idx|
+      if pair[0] == key
+         pair[1] = val
+         return [key, val]
+      end
+    end
+    @map << [key, val]
+  end
+
+  def lookup(key)
+    @map.each do |k, v|
+      return k, v if k == key
+    end
+  end
+
+  def remove(key)
+    @map.each do |pair|
+      @map.delete(pair) if pair[0] == key
+    end
+  end
+
+end
