8eae0d1dc7a6d2ce6c6988832969aead0a07c17e
Nate Reiners <nate.reiners@gmail.com>
2016-11-19 14:17:25 -0800

    complete simon game to be played in terminal

---
+++ W2D1/skeleton/lib/simon.rb
@@ -0,0 +1,68 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    system("cls")
+    if require_sequence != @seq
+      @game_over = true
+    else
+      round_success_message
+    end
+  end
+
+  def show_sequence
+    add_random_color
+    @sequence_length += 1
+    print "\n"
+    @seq.each do |col|
+      print " #{col} |"
+      sleep(1)
+    end
+    print "\n"
+  end
+
+  def require_sequence
+    col_abbrvs = %w(r b g y)
+    puts "enter the previous color sequence! (r b g y)"
+    seq_guess = gets.chomp.split("")
+    seq_guess.map { |char| COLORS[col_abbrvs.index(char)] }
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+    puts "Great Memory! Keep it up."
+  end
+
+  def game_over_message
+    puts "Game over, you lose!"
+  end
+
+  def reset_game
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
+  end
+end
+
+x = Simon.new
+x.play
