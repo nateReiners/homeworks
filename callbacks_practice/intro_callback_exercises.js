4e55444c1a3782ca7957e158fc3691e29f300048
Nate Reiners <nate.reiners@gmail.com>
2016-12-18 22:07:31 -0800

    add some recursion practice in addition to callback practice probs

--- callbacks_practice/intro_callback_exercises.js
+++ callbacks_practice/intro_callback_exercises.js
@@ -24,3 +24,37 @@ Array.prototype.two_sum = function() {
 
 
 // console.log([1, 2, -1000, -2, -1, 1, 1000].two_sum());
+Array.prototype.myTranspose = function() {
+  let transposedArr = []
+
+  for ( let i = 0; i < this.length; i++ ) {
+    let subarr = [];
+    for ( let j = 0; j < this.length; j++ ) {
+      subarr.push(this[j][i]);
+    }
+    transposedArr.push(subarr);
+    subarr = [];
+  }
+  return transposedArr;
+}
+
+
+// console.log([[1, 2, 3], [4, 5, 6], [7, 8, 9]].myTranspose());
+
+function range(start, end) {
+  if (start > end) {
+    return [];
+  }
+  return range(start, end - 1).concat(end);
+}
+
+function fibbonacci(n) {
+  if (n <= 2) {
+    return [0, 1].slice(0, n);
+  }
+  let fibs = fibbonacci(n - 1);
+  fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+  return fibs;
+}
+
+console.log(fibbonacci(90));
